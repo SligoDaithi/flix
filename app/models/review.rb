1254893a8a8f984ef27d08c72b21392e73a02180
David Sheerin <127449445+SligoDaithi@users.noreply.github.com>
2023-03-25 00:39:53 +0000

    adding reviews for has_many learnings

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :movie
+  validates :name, :stars, presence: true 
+  validates :stars, numericality: { in: 1..5, message: "must be between 1 and 5" }
+  validates :comment, length: { minimum: 4 }
 end
