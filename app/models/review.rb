7604be61a21417fbbeb13336b9d5f970c7cda1eb
David Sheerin <127449445+SligoDaithi@users.noreply.github.com>
2023-03-26 23:04:22 +0100

    Nested resources and adding star visuals

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,9 @@ class Review < ApplicationRecord
   belongs_to :movie
   validates :name, :stars, presence: true 
   validates :stars, numericality: { in: 1..5, message: "must be between 1 and 5" }
-  validates :comment, length: { minimum: 4 }
+  validates :comment, length: { minimum: 4 } 
+
+  def stars_as_percent 
+    (stars / 5.0 ) * 100.0
+  end
 end
