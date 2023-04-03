c5ccfa7daeb7d2391ae1639834882c65829e2c11
David Sheerin <127449445+SligoDaithi@users.noreply.github.com>
2023-04-03 21:59:33 +0100

    one to many forms

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :movie
   validates :name, :stars, presence: true 
-  validates :stars, numericality: { in: 1..5, message: "must be between 1 and 5" }
+  STARS = [ 1, 2, 3, 4, 5 ] 
+  validates :stars, inclusion: { in: STARS, message: "must be between 1 and 5" } 
   validates :comment, length: { minimum: 4 } 
 
   def stars_as_percent 
