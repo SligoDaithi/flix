af39f0ae8fd3fb99d77a2e4969dad20d5f96b542
David Sheerin <127449445+SligoDaithi@users.noreply.github.com>
2023-04-09 13:37:32 +0100

    Adding authentication to the signin form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,18 @@
 class SessionsController < ApplicationController
   def new
-
   end
 
   def create 
-    fail
+    user = User.find_by(email: params[:email_or_username]) || User.find_by(username: params[:email_or_username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back #{ user.name }!"
+    else
+      flash.now[:alert] = "Incorrect email/password combination!"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
-
   end
 end
