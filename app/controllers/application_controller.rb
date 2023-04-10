670177c446fab9abd850109263a0180f97ddad6a
David Sheerin <127449445+SligoDaithi@users.noreply.github.com>
2023-04-10 14:40:39 +0100

    authorisation - restricting access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   add_flash_types(:danger)
+
+  private 
+
+  def current_user 
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def require_signin
+    unless current_user
+      redirect_to new_session_url, notice: "Please sign in first."
+    end
+  end
+
+  helper_method :current_user 
 end
