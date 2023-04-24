674a47a86901f0b1f1f4e946ce9e11f8db9b40a5
David Sheerin <127449445+SligoDaithi@users.noreply.github.com>
2023-04-24 22:34:28 +0100

    file upload validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,9 +12,9 @@ class Movie < ApplicationRecord
   validates :released_on, :duration, presence: true 
   validates :description, length: { minimum: 25 }
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 } 
-
   RATINGS = %w(G PG PG-13 R NC-17) 
   validates :rating, inclusion: { in: RATINGS }
+  validate :acceptable_image 
 
   scope :released, lambda { where("released_on < ?", Time.now).order("released_on desc") }
   scope :upcoming, lambda { where("released_on > ?", Time.now).order("released_on asc") }
@@ -48,5 +48,16 @@ class Movie < ApplicationRecord
 
   def set_slug
     self.slug = title.parameterize 
+  end
+
+  def acceptable_image
+    return unless main_image.attached?
+    unless main_image.blob.byte_size <= 1.megabyte
+      errors.add(:main_image, "is too big")
+    end
+    acceptable_types = ["image/png", "image/jpeg", "image/jpg"]
+    unless acceptable_types.include?(main_image.blob.content_type) 
+      errors.add(:main_image, "is not a supported image type")
+    end
   end
 end
